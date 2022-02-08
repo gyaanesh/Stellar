69dc5cdc0d55b53bb1174ba2377715253a20edc6
ganesh chandra <gchandra4727@gmail.com>
2022-02-08 22:20:07 +0530

    header footer sidebar added with dhashboard

---
+++ admin/include/footer.php
@@ -0,0 +1,22 @@
+
+       </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="../vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="../vendors/select2/select2.min.js"></script>
+    <script src="../vendors/typeahead.js/typeahead.bundle.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="../js/off-canvas.js"></script>
+    <script src="../js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="../js/typeahead.js"></script>
+    <script src="../js/select2.js"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html>
